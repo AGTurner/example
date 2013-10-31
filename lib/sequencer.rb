b94e3af0e2dad666301bb77e97dde96e4f69a794
Andrew Turner <andrew.turner@melbourne.co.uk>
2013-10-31 13:58:46 +0000

    Get the sequencer returning collections without worrying about dependencies

--- lib/sequencer.rb
+++ lib/sequencer.rb
@@ -1,7 +1,13 @@
+require_relative 'jobs_parser'
+
 class Sequencer
 
   def sequence(jobs)
-    ''
+    return '' if jobs.empty?
+    sorted_jobs = []
+    jobs_array = JobsParser.parse(jobs)
+    jobs_array.each { |job_hash| job_hash.each { |job, dep| sorted_jobs << job } }
+    sorted_jobs
   end
-  
+
 end
--- test/sequencer_test.rb
+++ test/sequencer_test.rb
@@ -18,7 +18,34 @@ class SequencerTest < Test::Unit::TestCase
       should 'return an empty sequence' do
         assert_equal '', @sequencer.sequence(@jobs)
       end
+
+    end
+
+    context 'that is passed the job structure "a =>"' do
       
+      setup do
+        @jobs = 'a =>'
+      end
+
+      should 'return the sequence: a' do
+        assert_equal ['a'], @sequencer.sequence(@jobs)
+      end
+
+    end
+
+    context 'that is passed the job structure
+              a =>
+              b =>
+              c =>' do
+      
+      setup do
+        @jobs = "a =>\nb =>\nc =>"
+      end
+
+      should 'return the sequence: abc' do
+        assert_equal ['a','b','c'], @sequencer.sequence(@jobs)
+      end
+
     end
   end
 end
