cf884874abf1ea25e4fd32314c74ba4658423b0c
Andrew Turner <andrew.turner@melbourne.co.uk>
2013-10-31 14:14:02 +0000

    Refactor jobs parser and a add a test

--- lib/jobs_parser.rb
+++ lib/jobs_parser.rb
@@ -3,8 +3,8 @@ class JobsParser
   def self.parse(jobs)
     jobs.split("\n").map do |job|
       job_array = job.split("=>")
-      job_array.push '' if job_array.count == 1
       job_array.each { |j| j.strip! }
+      job_array.push nil if job_array.count == 1
       Hash[*job_array]
     end
   end
---
+++ test/jobs_parser_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class JobsParserTest < Test::Unit::TestCase
+  context 'The jobs parser' do
+
+    setup do
+      @jobs = "a =>\nb => c\nc =>"
+    end
+
+    should 'parse a valid job sequence into a hash' do
+      assert_equal [{"a" => nil}, {"b" => "c"}, {"c" => nil}], JobsParser.parse(@jobs)
+    end
+  end
+end
